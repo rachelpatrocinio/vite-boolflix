43d8f5729f5ce3fa48840074aa330b7157182a1e
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-04-16 15:42:24 +0200

    Added: hover on Card

--- src/components/CardMovieOrTv.vue
+++ src/components/CardMovieOrTv.vue
@@ -17,33 +17,82 @@ export default {
 <template>
     <li class="card col-3" v-for="movie in store.searchedMovies">
         <img :src="`${store.imgUrlBase}/${movie.poster_path}`" :alt="movie.title">
-        <h4>{{ movie.title }}</h4>
-        <h6>{{ movie.original_title }}</h6>
-        <img class="flag" v-if="movie.original_language === 'it'" src="../../public/italy.png">
-        <img class="flag" v-else-if="movie.original_language === 'en'" src="../../public/usa.png">
-        <img class="flag" v-else-if="movie.original_language === 'es'" src="../../public/spain.png">
-        <img class="flag" v-else-if="movie.original_language === 'de'" src="../../public/deutsch.png">
-        <p v-else> {{ movie.original_language }}</p>
-        <!-- <p>{{ Math.floor(movie.vote_average) }}</p> -->
-        <CardStars :vote="Math.floor(movie.vote_average)"/>
+        <div class="overlay">
+            <div>
+                <h3>{{ movie.title }}</h3>
+                <h6 class="mb-10">{{ movie.original_title }}</h6>
+                <p>{{ movie.overview }}</p>
+            </div>
+            <div>
+                <img class="flag" v-if="movie.original_language === 'it'" src="../../public/italy.png">
+                <img class="flag" v-else-if="movie.original_language === 'en'" src="../../public/usa.png">
+                <img class="flag" v-else-if="movie.original_language === 'es'" src="../../public/spain.png">
+                <img class="flag" v-else-if="movie.original_language === 'de'" src="../../public/deutsch.png">
+                <p v-else> {{ movie.original_language }}</p>
+                <CardStars :vote="Math.floor(movie.vote_average)"/>
+            </div>
+        </div>
     </li>
     <li class="card col-3" v-for="serietv in store.searchedTv">
         <img :src="`${store.imgUrlBase}/${serietv.poster_path}`" :alt="serietv.original_name">
-        <h4>{{ serietv.original_name }}</h4>
-        <h6>{{ serietv.first_air_date }}</h6>
-        <img class="flag" v-if="serietv.original_language === 'it'" src="../../public/italy.png">
-        <img class="flag" v-else-if="serietv.original_language === 'en'" src="../../public/usa.png">
-        <img class="flag" v-else-if="serietv.original_language === 'es'" src="../../public/spain.png">
-        <img class="flag" v-else-if="serietv.original_language === 'de'" src="../../public/deutsch.png">
-        <p v-else> {{ serietv.original_language }}</p>
-        <!-- <p>{{ Math.floor(serietv.vote_average) }}</p> -->
-        <CardStars :vote="Math.floor(serietv.vote_average)"/>
+        <div class="overlay">
+            <h4>{{ serietv.original_name }}</h4>
+            <h6>{{ serietv.first_air_date }}</h6>
+            <p>{{ serietv.overview }}</p>
+            <img class="flag" v-if="serietv.original_language === 'it'" src="../../public/italy.png">
+            <img class="flag" v-else-if="serietv.original_language === 'en'" src="../../public/usa.png">
+            <img class="flag" v-else-if="serietv.original_language === 'es'" src="../../public/spain.png">
+            <img class="flag" v-else-if="serietv.original_language === 'de'" src="../../public/deutsch.png">
+            <p v-else> {{ serietv.original_language }}</p>
+            <CardStars :vote="Math.floor(serietv.vote_average)"/>
+        </div>
     </li>
 </template>
 
 <style lang="scss" scoped>
+.overlay{
+    opacity: 1;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 10px;
+    padding: 5px;
+    background-color: rgba($color: black, $alpha: 0.9);
+    color:white;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
 .card{
     padding: 10px;
+    position: relative;
+
+    .overlay{
+        opacity: 0;
+    }
+
+    &:hover .overlay{
+    opacity: 1;
+    transition: 0.7s;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 10px;
+    padding: 5px;
+    background-color: rgba($color: black, $alpha: 0.9);
+    color:white;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    }
+
     img.flag{
         width: 30px;
     }
