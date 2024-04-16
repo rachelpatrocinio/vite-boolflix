3c3dcb65fe90a1b410451900e054195907d58e39
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-04-16 11:42:16 +0200

    Added: store.js

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <script>
+import { store } from './store.js'
+
+export default{
+    data(){
+        return{
+            store
+        }
+    }
+}
 </script>
 
 <template>
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue';
+
+export const store = reactive ({
+    API_KEY: 'f8ca6616a1fe0e3970340e21271f15ba',
+    API_MOVIE:'https://api.themoviedb.org/3/search/movie',
+    API_TV: 'https://api.themoviedb.org/3/search/tv'
+})
