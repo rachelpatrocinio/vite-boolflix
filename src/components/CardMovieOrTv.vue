5c5a9682769f4c9d654bce7fa0e3673af252ca28
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-04-16 13:16:01 +0200

    Added: original_language flags

--- src/components/CardMovieOrTv.vue
+++ src/components/CardMovieOrTv.vue
@@ -15,7 +15,11 @@ export default {
     <li class="card col-3" v-for="movie in store.searchedMovies">
         <h4>{{ movie.title }}</h4>
         <h6>{{ movie.original_title }}</h6>
-        <p>{{ movie.originial_language }}</p>
+        <img v-if="movie.original_language === 'it'" src="../../public/italy.png">
+        <img v-else-if="movie.original_language === 'en'" src="../../public/usa.png">
+        <img v-else-if="movie.original_language === 'es'" src="../../public/spain.png">
+        <img v-else-if="movie.original_language === 'de'" src="../../public/deutsch.png">
+        <p v-else> {{ movie.original_language }}</p>
         <p>{{ movie.vote_average }}</p>
     </li>
 </template>
@@ -23,6 +27,8 @@ export default {
 <style lang="scss" scoped>
 .card{
     padding: 10px;
+    img{
+        width: 30px;
+    }
 }
-
 </style>
